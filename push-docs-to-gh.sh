33c06f243c9ea3345da1afb035bb0daa5de76c35
Gaston Moreno <gastonadrian@gmail.com>
2016-11-21 22:29:48 -0300

    deploying documentation

---
+++ push-docs-to-gh.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "gastonadrian/cmi-data-collector" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing documentation...\n"
+
+  cp -R docs $HOME/docs-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/${$TRAVIS_REPO_SLUG} gh-pages > /dev/null
+
+  cd gh-pages
+  git rm -rf ./docs
+  cp -Rf $HOME/docs-latest ./docs
+  git add -f .
+  git commit -m "Latest documentation on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published documentation to gh-pages.\n"
+  
+fi
